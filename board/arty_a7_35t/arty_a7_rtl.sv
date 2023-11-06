9bd27e7f52b352de949d34c0712a15bfc0dd87b6
abigsam <abigsam@gmail.com>
2023-11-06 21:48:07 +0200

    Add constraints and empty RTL

--- board/arty_a7_35t/arty_a7_rtl.sv
+++ board/arty_a7_35t/arty_a7_rtl.sv
@@ -2,8 +2,15 @@
 
 
 module arty_a7_rtl (
-
+    input  logic CLK100MHZ,
+    input  logic [3:0] btn,
+    input  logic [3:0] sw,
+    output logic [3:0] led,
+    output logic [7:0] ja
 );
+
+
+
 
 
 endmodule
